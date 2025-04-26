40db74ab24cde9b6e66ce76384f06004603e4283
cabf05 <cabf05@gmail.com>
2025-04-26 16:56:29 -0400

    Delete pages directory

--- pages/index.jsx
+++
@@ -1,7 +0,0 @@
-// pages/index.jsx
-import dynamic from 'next/dynamic'
-
-// dynamically load the Builder component only on the client
-export default dynamic(() => import('../components/Builder'), {
-  ssr: false,
-})
